23b270c73db0911dfea6c2cddd2ad3341672ade9
YagishitaAkihiro <yagishitaakihiro@okadanet.org>
2018-02-06 14:13:35 +0900

    update

--- scripts/data/add_mode.py
+++ scripts/data/add_mode.py
@@ -4,9 +4,13 @@
 def main():
     f=open("/home/roboworks/catkin_ws/src/myo2dynamixel/scripts/data/train.txt","w")
     for line in open("/home/roboworks/catkin_ws/src/myo2dynamixel/scripts/data/open.txt" , "r"):
+        line = line.replace('(', '')
+        line = line.replace(')', '')
         f.writelines("1, " + line)
     
     for line in open("/home/roboworks/catkin_ws/src/myo2dynamixel/scripts/data/close.txt", "r"):
+        line = line.replace('(', '')
+        line = line.replace(')', '')
         f.writelines("0, " + line)
     
     print("Finish!")
--- scripts/svm_hand_test.py
+++ scripts/svm_hand_test.py
@@ -9,7 +9,7 @@ from std_msgs.msg import Float64
 class SVM_HC():
       def __init__(self):
           rospy.init_node("svm_hc")
-
+          print "learning phase"
           # 学習データ
           data_training_tmp = np.loadtxt('/home/roboworks/catkin_ws/src/myo2dynamixel/scripts/data/train.txt', delimiter=',')
           data_training = [[x[1], x[2],x[3], x[4],x[5], x[6],x[7], x[8]] for x in data_training_tmp]
@@ -17,6 +17,7 @@ class SVM_HC():
           # 学習
           self.estimator = LinearSVC(C=1)#誤差項のペナルティパラメータC よくわからん。
           self.estimator.fit(data_training, label_training)
+          print "starting node"
           self.pub = rospy.Publisher("/tilt_controller/command", Float64, queue_size=1)
           rospy.Subscriber("/myo_emg", EmgArray, self.call, queue_size=1)
           rospy.spin()
@@ -29,12 +30,12 @@ class SVM_HC():
           
           # 予測するよー
           label_prediction = self.estimator.predict(data_test)
-          if label_prediction == 0:
-#             print "開いてる"
-             self.pub.publish(1)
-          else:
-#             print "閉じてる"
+          if label_prediction == 1:
+             print "開いてる"
              self.pub.publish(0)
+          else:
+             print "閉じてる"
+             self.pub.publish(1)
 #          print(label_prediction)
 
 if __name__ == "__main__":
